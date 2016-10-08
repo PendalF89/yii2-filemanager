f0811d8db84e5da49ca8f6603b11502c931c1e31
Shah Typhoon <shah.typhoon@gmail.com>
2016-10-08 18:38:00 +0400

    Update MediafileBehavior.php
    Для правильного поиска модели и в целом, функционирования поведения,  в loadModel необходимо передавать условие поиска по колонке `url`: ['url' => $this->owner->$attr]
    В View можно будет использовать:
    <?= $model->imageURL('small') ?>

--- behaviors/MediafileBehavior.php
+++ behaviors/MediafileBehavior.php
@@ -5,6 +5,7 @@ use yii;
 use yii\base\Behavior;
 use yii\db\ActiveRecord;
 use pendalf89\filemanager\models\Mediafile;
+use pendalf89\filemanager\models\Owners;
 
 class MediafileBehavior extends Behavior
 {
@@ -37,7 +38,7 @@ class MediafileBehavior extends Behavior
     public function addOwners()
     {
         foreach ($this->attributes as $attr) {
-            if ($mediafile = $this->loadModel($this->owner->$attr)) {
+            if ($mediafile = $this->loadModel(['url' => $this->owner->$attr])) {
                 $mediafile->addOwner($this->owner->primaryKey, $this->name, $attr);
             }
         }
@@ -51,7 +52,7 @@ class MediafileBehavior extends Behavior
         foreach ($this->attributes as $attr) {
             Mediafile::removeOwner($this->owner->primaryKey, $this->name, $attr);
 
-            if ($mediafile = $this->loadModel($this->owner->$attr)) {
+            if ($mediafile = $this->loadModel(['url' => $this->owner->$attr])) {
                 $mediafile->addOwner($this->owner->primaryKey, $this->name, $attr);
             }
         }
@@ -66,6 +67,8 @@ class MediafileBehavior extends Behavior
             Mediafile::removeOwner($this->owner->primaryKey, $this->name, $attr);
         }
     }
+    
+    
 
     /**
      * Load model by id
@@ -75,5 +78,31 @@ class MediafileBehavior extends Behavior
     private function loadModel($id)
     {
         return Mediafile::findOne($id);
+    }
+    
+    /**
+     * Возвращает ссылку на миниатюру, заданную через аргументы $attribute и $alias
+     * @param string $alias Наименование миниатюры, задается в конфигурации filemanager
+     * @param bool|string $attribute Наименование атрибута (на случай, если в поведении 
+     *                               казано несколько атрибутов)
+     * @return bool|string Вернет false, если нет атрибутов или указанный не найден.
+     */
+    public function imageURL($alias, $attribute = false) {
+        if ($attribute && !in_array($attribute, $this->attributes)) {
+            return false;
+        } elseif ($attribute === false && !($attribute = reset($this->attributes))) {
+            return false;
+        }
+        $owner = Owners::findOne([
+            'owner_id' => $this->owner->primaryKey,
+            'owner' => $this->name,
+            'owner_attribute' => $attribute,
+        ]);
+        if ($owner instanceof Owners) {
+            if ($mediaFile = Mediafile::findOne($owner->mediafile_id)) {
+                return $mediaFile->getThumbUrl($alias);
+            }
+        }
+        return false;
     }
 }
